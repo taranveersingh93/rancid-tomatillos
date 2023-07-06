6b71b462e49f7443399672988b979fdef14b806b
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-07-06 19:34:39 -0400

    Add a navbar with styling.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import movieData from './data';
 import Movies from './Movies'
+import Navbar from './Navbar';
 import './App.css';
 import { useState } from 'react';
 
@@ -8,11 +9,12 @@ function App() {
   // make a movie card, pass in props - return the card structure in JSX
   // iterate through the moviesData array, returning the data as cards, assign props in JSX
 
-  const [moviesList, setMovies] = useState(movieData.movies);
+  const [movies, setMovies] = useState(movieData.movies);
 
   return (
     <div className="App">
-      <Movies movies={moviesList}/>
+      <Navbar />
+      <Movies movies={movies}/>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import './Navbar.css'
+
+function Navbar(){
+  return (
+    <div className='navbar'>
+      <div className='user-info'>
+        Welcome, user!
+      </div>
+      <h1 className='main-title'>
+        MainTitle
+      </h1>
+    </div>
+  )
+}
+
+export default Navbar;
