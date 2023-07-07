a3a7ead6f29a217b8976533d4156de9626cec119
Taranveer Singh <taranveersingh93@gmail.com>
2023-07-06 21:26:17 -0400

    Arrange navbar as flex

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,14 +4,14 @@ import tomatoIcon from '../../images/tomato.png'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <div className='user-info'>
-        Welcome, user!
-      </div>
       <div className='logo-header'>
         <img src={tomatoIcon} className="tomato-logo flicker-slow" alt="logo" />
         <p className='neon'>
-          RANCID TO<span className="flicker-fast">M</span>A<span className="flicker-fast">T</span>OES
+          RANCID TO<span className="flicker-fast">M</span>A<span className="flicker-slow">T</span>OES
         </p>
+      </div>
+      <div className='user-info'>
+        Welcome, user!
       </div>
     </div>
   )
