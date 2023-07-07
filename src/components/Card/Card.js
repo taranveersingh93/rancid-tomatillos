e406da784592626039385d4f6b1069ac45ad9669
Taranveer Singh <taranveersingh93@gmail.com>
2023-07-06 23:58:10 -0400

    Make scroll animation

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,13 @@
 import './Card.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
+
+AOS.init();
 
 const Card = ({ movieTitle, moviePoster, movieYear, id }) => {
   return (
-    <div className='movie-card'>
+    <div className='movie-card' data-aos="fade-up">
       <div className='movie-card-image-container'>
         <img src={moviePoster} className='movie-poster' alt={movieTitle} />
       </div>
