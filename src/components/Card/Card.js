62995400fefe0bce5ca6f36e5f196917751101bf
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-07-06 20:32:19 -0400

    Organize components in individual directories.

---
+++ src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import './Card.css';
+
+const Card = ({ movieTitle, moviePoster }) => {
+  return (
+    <div className='movie-card'>
+      <img src={moviePoster} className='movie-poster' alt={movieTitle} />
+      <h3>
+        {movieTitle}
+      </h3>
+    </div>
+  )
+}
+
+export default Card;
