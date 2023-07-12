73ed12cb62a24259d2dc0e7d728283bbd37d9db8
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-07-12 01:02:57 -0400

    Separate test into multiple it blocks, accounting for testing needs.

--- cypress/e2e/home-spec.cy.js
+++ cypress/e2e/home-spec.cy.js
@@ -7,15 +7,24 @@ describe('rancid tomatillos homepage user flows', () => {
     cy.visit('http://localhost:3000');
   });
 
-  it('should confirm that the right elements are displayed upon navigating to the app', () => {
+  it('should confirm that all navbar elements are displayed upon navigating to the app', () => {
     // cy.wait(500)
     // cy.get('wait.input1').contains('movie-card-image-container')
     cy.contains('h1', 'RANCID')
     cy.contains('h2', 'TOMATILLOS')
+    cy.get('img[name=tomato-image]')
+      .should('be.visible')
     cy.contains('div', 'Welcome, user!')
-    cy.get('input[name=searchbar]')
-    cy.get('img[name=cross-icon]')
+  });
+  
+  it('should display all movie posters', () => {
     cy.get('.all-movies-container').children()
       .should('have.length', 40)
-  })
-})
+  });
+  
+  it('should display filter ribbon', () => {
+    cy.get('input[name=searchbar]')
+    cy.get('img[name=cross-icon]')
+      .should('be.visible')
+  });
+});
