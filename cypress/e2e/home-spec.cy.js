e1ec832ddce649450b4e8e0e9dfb87fccff84cca
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-07-12 09:16:45 -0400

    Add intercept to beforeEach hook.

--- cypress/e2e/home-spec.cy.js
+++ cypress/e2e/home-spec.cy.js
@@ -1,30 +1,34 @@
+import { Link } from "react-router-dom";
+
 describe('rancid tomatillos homepage user flows', () => {
 
   beforeEach(() => {
-    // cy.intercept("GET", "http://localhost:3000", {
-    //   statusCode: 200,
-    // })
-    cy.visit('http://localhost:3000');
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 200,
+      fixture: '.././fixtures/all-movies-test.json'
+    })
+      .visit('http://localhost:3000');
   });
-
-  it('should confirm that all navbar elements are displayed upon navigating to the app', () => {
-    // cy.wait(500)
-    // cy.get('wait.input1').contains('movie-card-image-container')
+  
+  it('should display all navbar elements upon navigating to the app', () => {
     cy.contains('h1', 'RANCID')
     cy.contains('h2', 'TOMATILLOS')
     cy.get('img[name=tomato-image]')
-      .should('be.visible')
+    .should('be.visible')
     cy.contains('div', 'Welcome, user!')
-  });
-  
-  it('should display all movie posters', () => {
-    cy.get('.all-movies-container').children()
-      .should('have.length', 40)
   });
   
   it('should display filter ribbon', () => {
     cy.get('input[name=searchbar]')
     cy.get('img[name=cross-icon]')
-      .should('be.visible')
+    .should('be.visible')
+  });
+  
+  it('should display all movie posters', () => {
+    cy.get('.all-movies-container').children()
+      .should('have.length', 4)
+    cy.get('img[name=movie-poster]')
+      .get('#436270')
+    cy.contains('h3', 'Black Adam (2022)')
   });
 });
