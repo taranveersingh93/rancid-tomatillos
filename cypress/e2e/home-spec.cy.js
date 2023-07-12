2bdb9b217d13499b14521402bc30f3b81e9ccbab
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-07-11 21:00:48 -0400

    Add first test, for h1 element on homepage.

---
+++ cypress/e2e/home-spec.cy.js
@@ -0,0 +1,16 @@
+describe('rancid tomatillos homepage user flows', () => {
+
+  beforeEach(() => {
+    // cy.intercept("GET", "http://localhost:3000", {
+    //   statusCode: 200,
+    // })
+    cy.visit('http://localhost:3000');
+  });
+
+  it('should confirm that the right elements are displayed upon navigating to the app', () => {
+    // cy.wait(500)
+    // cy.get('wait.input1').contains('movie-card-image-container')
+    cy.contains('h1', 'RANCID')
+    cy.contains('h2', 'TOMATI')
+  })
+})
