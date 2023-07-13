4913b390ff1f79fab26b9cc1d67b0eb1341ce972
Taranveer Singh <taranveersingh93@gmail.com>
2023-07-13 18:19:42 -0400

    Link logo to homepage

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,23 @@
 import './Navbar.css'
 import tomatoIcon from '../../images/tomato.png'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <div className='logo-header'>
-        <img src={tomatoIcon} className="tomato-logo flicker-normal" name="tomato-image" alt="logo" />
-        <div className='logo-text'>
-          <h1 className='neon'>
-            RANCID
-          </h1>
-          <h2 className='neon'>
-            TOMATI<span className="flicker-fast">L</span><span className="flicker-slow">L</span>OS
-          </h2>
+      <Link to='/'>
+        <div className='logo-header'>
+          <img src={tomatoIcon} className="tomato-logo flicker-normal" name="tomato-image" alt="logo" />
+          <div className='logo-text'>
+            <h1 className='neon'>
+              RANCID
+            </h1>
+            <h2 className='neon'>
+              TOMATI<span className="flicker-fast">L</span><span className="flicker-slow">L</span>OS
+            </h2>
+          </div>
         </div>
-      </div>
+      </Link>
       <div className='user-info'>
         Welcome, user!
       </div>
